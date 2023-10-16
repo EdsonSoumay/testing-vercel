ff487c3c3f0d94056bf54c456c3616870cb81059
EdsonSoumay <s21810088@student.unklab.ac.id>
2023-10-16 10:06:12 +0900

    tetslt

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("Express vercel app response")
+})
+app.listen(5000, ()=>{console.log("app is running")})
